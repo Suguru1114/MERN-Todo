b178626e75988e4fd0ac37f488f2e341ab5771a1
Suguru Miyata <suguru1114@gmail.com>
2025-07-21 00:08:59 +1000

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express"
+// const express = require("express");
+
+const app = express();
+
+ 
+
+app.get("/api/notes", (req, res) => {
+    res.status(200).send("you got 15 notes");
+});
+
+app.post("/api/notes", (req, res) => {
+    res.status(201).json({message:"post created successfully"});
+});
+
+app.put("/api/notes:id", (req, res) => {
+    res.status(200).json({message:"post updated successfully"});
+});
+
+app.delete("/api/notes:id", (req, res) => {
+    res.status(200).json({message:"Note deleted successfully"});
+});
+
+app.listen(5001, ()=> {
+
+    console.log("Server started on port 5001");
+});
