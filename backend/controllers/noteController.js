e1f276557a2d01217f1ce2dce6de5f1a5e9f0a24
Suguru Miyata <suguru1114@gmail.com>
2025-07-28 23:17:01 +1000

    controller

---
+++ backend/controllers/noteController.js
@@ -0,0 +1,8 @@
+// const getAllNotes = (req, res) => {
+//     res.status(200).send("you got 15 notes");
+// };
+
+export const function getAllNotes(req, res){
+    res.status(200).send("you got 15 notes");
+}
+
