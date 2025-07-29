b96c1260a41d24c379a005e303227c4fe90a319e
Suguru Miyata <suguru1114@gmail.com>
2025-07-30 06:02:08 +1000

    create router file

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -2,7 +2,10 @@
 //     res.status(200).send("you got 15 notes");
 // };
 
-export const function getAllNotes(req, res){
+export function getAllNotes(req, res){
     res.status(200).send("you got 15 notes");
 }
 
+export function createNote(req, res){
+    res.status(200).json({message:"Note created successfully"});
+}
--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,22 +1,12 @@
 import express from "express";
+import {deleteNotes, getAllNotes} from "../controllers/noteController.js";
 
 const router = express.Router();
 
-router.get("/", (req,res) => {
-    res.status(200).send("you got 15 notes");
-});
-
-router.post("/", (req,res) => {
-    res.status(201).json({message:"post created succesfuly"});
-});
-
-router.put("/:id", (req, res) => {
-    res.status(200).json({message:"post updated successfully"});
-});
-
-router.delete("/ :id", (req,res)=> {
-    res.status(200).json({message:"Note deleted successfully"});
-});
+router.get("/", getAllNotes);
+router.post("/", createAllNotes);
+router.put("/:id", updateAllNotes);
+router.delete("/:id", deleteNotes);
 
 export default router;
 
